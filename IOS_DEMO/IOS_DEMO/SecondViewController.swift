428c3916043855b8829d0b59da1fe9855a68bd50
Jay Buddhdev <jay.b@simformsolutions.com>
2022-01-24 18:44:19 +0530

    TE5-312_Swift_Exercise_Completed

---
+++ IOS_DEMO/IOS_DEMO/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  IOS_DEMO
+//
+//  Created by Jay Buddhdev on 27/01/22.
+//
+
+import UIKit
+
+protocol TextDelegate : AnyObject{
+    func sendText(text : String)
+}
+class SecondViewController: UIViewController {
+    
+    // MARK: - Outlet
+    @IBOutlet weak var txtFlied: UITextField!
+    weak var txtDelegate : TextDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+// MARK: - Outlet Action
+extension SecondViewController {
+    @IBAction func goToSecondViewController(_ sender: UIButton) {
+        txtDelegate?.sendText(text: txtFlied.text ?? "Hello")
+        dismiss(animated:true, completion: nil)
+    }
+}
