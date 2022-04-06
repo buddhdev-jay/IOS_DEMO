2f5f66a34c938cb7f4f8388ed920d81620053f36
Jay Buddhdev <jay.b@simformsolutions.com>
2022-04-06 15:35:57 +0530

    TE5-T579 Without Navigation controller

--- IOS_DEMO/IOS_DEMO/UIComponents/Signup_Screen_ViewController.swift
+++ IOS_DEMO/IOS_DEMO/UIComponents/Signup_Screen_ViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class Signup_Screen_ViewController: UIViewController {
     
-    // MARK: -  Variables
+    // MARK: - Variables
     var timer: Timer?
     
     // MARK: - Outlet
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var stepper: UIStepper!
     @IBOutlet weak var lblSteppervalue: UILabel!
     @IBOutlet weak var progressView: UIProgressView!
@@ -21,25 +23,29 @@ class Signup_Screen_ViewController: UIViewController {
     @IBOutlet weak var lblAgeNumber: UILabel!
     @IBOutlet weak var ageSlider: UISlider!
     
-    // MARK: -  View Lifecycle
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
 // MARK: - Outlet Action
-extension Signup_Screen_ViewController{
+extension Signup_Screen_ViewController {
+    
     @IBAction func displayGender(_ sender: Any) {
         lblGender.text = genderSwitch.isOn ? Constants.genderMale :Constants.genderFemale
     }
+    
     @IBAction func displayAge(_ sender: Any) {
         lblAgeNumber.text = "\(Int(ageSlider.value))"
     }
+    
     @IBAction func stepperValue(_ sender: UIStepper) {
         lblSteppervalue.text = (stepper.value).description
     }
+    
     @IBAction func startProgressBar(_ sender: UIButton) {
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { timer in
+        timer = Timer.scheduledTimer(withTimeInterval: Constants.onePointZero, repeats: true, block: { timer in
             let change: Float = Float(Constants.zeroPointOne)
             self.progressView.progress = self.progressView.progress + (change)
             if self.progressView.progress >= Float(Constants.onePointZero) {
--- IOS_DEMO/IOS_DEMO/UIComponents/UISegmentedViewController.swift
+++ IOS_DEMO/IOS_DEMO/UIComponents/UISegmentedViewController.swift
@@ -11,14 +11,17 @@ class UISegmentedViewController: UIViewController {
     
     // MARK: - Outlet
     @IBOutlet weak var imageView: UIImageView!
+    
     // MARK: -  View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Ui Segment Control"
     }
 }
 
 // MARK: - Outlet Action
 extension UISegmentedViewController {
+    
     @IBAction func imageChange(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex{
         case Constants.ZERO:
@@ -30,5 +33,3 @@ extension UISegmentedViewController {
         }
     }
 }
-
-
