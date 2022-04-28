e3bec42a05afe491008620c6bf8a4b8ff36a7655
Jay Buddhdev <jay.b@simformsolutions.com>
2022-04-28 13:41:46 +0530

    Custom Control done 90%

--- IOS_DEMO/IOS_DEMO/Custom Controls/ImageTextField.swift
+++ IOS_DEMO/IOS_DEMO/Custom Controls/ImageTextField.swift
@@ -7,7 +7,67 @@
 
 import UIKit
 
+@IBDesignable
 class ImageTextField : UITextField {
     
+    override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+            var textRect = super.leftViewRect(forBounds: bounds)
+            textRect.origin.x += leftPadding
+            return textRect
+        }
+    override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+        var textRect = super.rightViewRect(forBounds: bounds)
+        textRect.origin.x -= rightPadding
+        return textRect
+    }
+    
+    @IBInspectable var leftPadding: CGFloat = 0
+    @IBInspectable var rightPadding: CGFloat = 0
+    @IBInspectable var leftImageMargin: CGFloat = 0
+    @IBInspectable var rightImageMargin: CGFloat = 0
+    @IBInspectable var rightImage: UIImage? {
+            didSet {
+              updateRightView()
+            }
+        }
+    @IBInspectable var leftImage: UIImage? {
+            didSet {
+                updateLeftView()
+            }
+        }
+    
+    func updateLeftView() {
+           if let image = leftImage {
+               leftViewMode = UITextField.ViewMode.always
+               let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+               imageView.contentMode = .center
+               let resizedImage = image.resized(to: CGSize(width: 20, height: self.frame.height - leftImageMargin))
+               imageView.image = resizedImage
+               leftView = imageView
+           } else {
+               leftViewMode = UITextField.ViewMode.never
+               leftView = nil
+           }
+       }
+    func updateRightView() {
+           if let image = rightImage {
+               rightViewMode = UITextField.ViewMode.always
+               let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+               imageView.contentMode = .center
+               let resizedImage = image.resized(to: CGSize(width: 20, height: self.frame.height - rightImageMargin))
+               imageView.image = resizedImage
+               rightView = imageView
+           } else {
+               rightViewMode = UITextField.ViewMode.never
+               rightView = nil
+           }
+       }
 }
 
+extension UIImage {
+    func resized(to size: CGSize) -> UIImage {
+            return UIGraphicsImageRenderer(size: size).image { _ in
+                draw(in:CGRect(origin: .zero, size: size))
+            }
+        }
+}
